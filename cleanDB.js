3588128750d5af84f3deb48d3f74d2c39f0ca4c1
KhaiPhamdevbase <khai.pham@devbase.us>
2023-04-13 17:34:44 +0700

    setup

---
+++ cleanDB.js
@@ -0,0 +1,42 @@
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
+
+
+async function main(){
+  const client = new MongoClient(process.env.DATABASE_UNWRITTEN);
+
+  try {
+    await client.connect();
+    const db = client.db('unwritten');
+    // await  listDatabases(client);
+
+    await cleanCollection(db, 'sender_answer');
+    await cleanCollection(db, 'sender_question');
+    await cleanCollection(db, 'sender_record_question');
+    await cleanCollection(db, 'receiver_record_answer');
+
+  } catch (e) {
+    console.error(e);
+  } finally {
+    await client.close();
+  }
+}
+
+async function listDatabases(client){
+    databasesList = await client.db().admin().listDatabases();
+    console.log("Databases:");
+    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+};
+
+async function cleanCollection(db, collectionName) {
+  try {
+    const result = await db.collection(collectionName).deleteMany({});
+    console.log(`${result.deletedCount} documents were deleted from the ${collectionName} collection.`);
+    return result;
+  } catch (e) {
+    console.error(e);
+   }
+}
+
+main().catch(console.error);
+
---
+++ readme.md
@@ -0,0 +1,15 @@
+## **Clone Source code**
+```bash
+will update
+```
+## **INSTALLATION**
+At the root folder
+setup .env
+```bash
+npm install
+```
+## **Run Script**
+At the root fodler
+```bash
+node cleanDB.js
+```
