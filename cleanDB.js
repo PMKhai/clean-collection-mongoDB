33475169bda7c95b150275cf21d0eec98b132274
KhaiPhamdevbase <khai.pham@devbase.us>
2023-06-17 11:24:22 +0700

    upgrate package and refactor code

--- cleanDB.js
+++ cleanDB.js
@@ -1,27 +1,31 @@
 const { MongoClient } = require('mongodb');
 require('dotenv').config();
 
-
 async function main(){
-  const client = new MongoClient(process.env.DATABASE_UNWRITTEN);
+    const client = new MongoClient(process.env.DATABASE_UNWRITTEN);
 
-  try {
-    await client.connect();
-    const db = client.db('unwritten');
-    // await  listDatabases(client);
+    try {
+        await client.connect();
+        const db = client.db('unwritten');
+        await  listDatabases(client);
+    
+        const collections = [
+            'sender_answer',
+            'sender_question',
+            'sender_record_question',
+            'receiver_record_answer',
+            'receiver_record_follow_up', 
+            'sender_record_follow_up'
+        ];
 
-    await cleanCollection(db, 'sender_answer');
-    await cleanCollection(db, 'sender_question');
-    await cleanCollection(db, 'sender_record_question');
-    await cleanCollection(db, 'receiver_record_answer');
-    await cleanCollection(db, 'receiver_record_follow_up');
-    await cleanCollection(db, 'sender_record_follow_up');
-
-  } catch (e) {
-    console.error(e);
-  } finally {
-    await client.close();
-  }
+        collections.forEach(async (collection) => {
+            await cleanCollection(db, collection);
+        })
+    } catch (e) {
+        console.error(e);
+    } finally {
+        await client.close();
+    }
 }
 
 async function listDatabases(client){
@@ -41,4 +45,3 @@ async function cleanCollection(db, collectionName) {
 }
 
 main().catch(console.error);
-
