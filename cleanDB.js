622169ae3f42ff6bdb7fd4aa7b7fece005c283bc
KhaiPhamdevbase <khai.pham@devbase.us>
2023-08-18 14:49:19 +0700

    feat: delete users by email

--- cleanDB.js
+++ cleanDB.js
@@ -7,20 +7,22 @@ async function main(){
     try {
         await client.connect();
         const db = client.db('unwritten');
-        await  listDatabases(client);
-    
-        const collections = [
-            'sender_answer',
-            'sender_question',
-            'sender_record_question',
-            'receiver_record_answer',
-            'receiver_record_follow_up', 
-            'sender_record_follow_up'
-        ];
+        // await  listDatabases(client);
 
-        collections.forEach(async (collection) => {
-            await cleanCollection(db, collection);
-        })
+        // const collections = [
+        //     'sender_answer',
+        //     'sender_question',
+        //     'sender_record_question',
+        //     'receiver_record_answer',
+        //     'receiver_record_follow_up', 
+        //     'sender_record_follow_up'
+        // ];
+
+        // collections.forEach(async (collection) => {
+        //     await cleanCollection(db, collection);
+        // })
+
+        await deleteUserByEmail(db, 'khai.pham')
     } catch (e) {
         console.error(e);
     } finally {
@@ -42,6 +44,16 @@ async function cleanCollection(db, collectionName) {
   } catch (e) {
     console.error(e);
    }
+}
+
+async function deleteUserByEmail(db, email) {
+  try {
+    const result = await db.collection('users').deleteMany({email: {$regex: `^${email}`}})
+    console.log(`${result.deletedCount} documents were deleted.`);
+    return result
+  } catch (e) {
+    console.log(e)
+  }
 }
 
 main().catch(console.error);
